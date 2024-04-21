bef54e3c28a12642f50647b5cfdd1a61b7ba51af
MathVictor <matheusvictor@alunos.utfpr.edu.br>
2024-04-21 00:03:33 -0300

    Ver 0.0.1: So um teste

--- nossoapp/App.js
+++ nossoapp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sexorr</Text>
+      <Text>Tom é gato, e isso não é uma propaganda</Text>
       <StatusBar style="auto" />
     </View>
   );
