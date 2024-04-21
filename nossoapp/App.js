9a0a48f30ad1ff62e861d11490459fe59b3c8ebc
MathVictor <matheusvictor@alunos.utfpr.edu.br>
2024-04-21 00:22:59 -0300

    Essa e a minha versao fdp

--- nossoapp/App.js
+++ nossoapp/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Tom é gato, e isso não é uma propaganda</Text>
       <Text>Tom nao é, mudei de ideia</Text>
+      <Text>Sim era uma propaganda, o tom é gato</Text>
       <StatusBar style="auto" />
     </View>
   );
