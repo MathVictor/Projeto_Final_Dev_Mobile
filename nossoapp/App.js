da75c7e33aa0864747faaf91796091693ba113a3
MathVictor <matheusvictor@alunos.utfpr.edu.br>
2024-04-21 15:44:27 -0300

    Ver. 0: Bora resetar o mind tomas

--- nossoapp/App.js
+++ nossoapp/App.js
@@ -4,61 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tom é gato, e isso não é uma propaganda</Text>
-      <Text>Tom nao é, mudei de ideia</Text>
-      <Text>Sim era uma propaganda, o tom é gato</Text>
-      <text>NAO TOM NAAAAAAAAAAO</text>
-      MEU P** TA MOLE
-      SUSHI É BOM, TOM É RUIM
-      TOM É MAMAS SIIIIIIM
+      <Text>Teste do Teste</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-// EU SOU O DONO DOS COMENTS SEU MERDA
-
-ashashda
-aisi
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Tom é gato, e isso não é uma propaganda</Text>
-      <Text>Tom nao é, mudei de ideia</Text>
-      <Text>Sim era uma propaganda, o tom é gato</Text>
-      <text>NAO TOM NAOOOOO</text>
-      MEU P** TA MUITO MOLE
-      APENAS MOLE
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-// Your mother is mine //
-// Alteração maluca da cabeça //
-// Testão
-// Tesão
-// Tesão 5
-// Tesão π
-// Tesão Oishii 🍣🥢
-
-// ALHO PORÓ
-// MAMA MINHA MAMA
-
-ashashda
-aisi
 const styles = StyleSheet.create({
   container: {
     flex: 1,
