512d2676307311a9cb83c71653c6be77281c4d48
Matheus <matheusvictor@alunos.utfpr.edu.br>
2024-04-22 16:19:05 -0300

    Ver 0.1: Finalmente alguma coisa funcionou nas nossas vidas

---
+++ nossoapp/src/screens/coleta.js
@@ -0,0 +1,7 @@
+import{View, StyleSheet} from 'react-native';
+import{useNavigation} from '@react-navigation/native'
+
+export default function Coleta(){
+    const navigation = useNavigation();
+    
+}
