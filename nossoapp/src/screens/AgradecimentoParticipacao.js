c1e84dd6dc031dca257fde83f449671eae0279f4
Matheus <matheusvictor@alunos.utfpr.edu.br>
2024-04-22 16:56:03 -0300

    Ver. 0.2: Tela de AgradecimentoParticipacao Pronta(Na teoria)

--- nossoapp/src/screens/AgradecimentoParticipacao.js
+++ nossoapp/src/screens/AgradecimentoParticipacao.js
@@ -1,18 +1,43 @@
-import { View,StyleSheet,Text } from "react-native";
-import{useNavigation} from '@react-navigation/native'
-
+import { useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 export default function AgradecimentoParticipacao(){
     const navigation = useNavigation();
-    return(
-        <View style={styles.View}>
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.pop();
+        }, 3000)
+        
+        return () => clearTimeout(timer)
+    }, [])
+
+    return (
+        <View style={styles.View}>  
+            <View style={styles.Agradecer}>
+                <Text style={styles.Text}>Obrigado por participar da pesquisa!</Text>
+                <Text style={styles.Text}>Aguardamos você no proximo ano!</Text>
+            </View>
         </View>
     )
+
 }
 
 const styles = StyleSheet.create({
     View: {
         backgroundColor: '#372775',
+        flex: 1,
+    },
+    Agradecer: {
+        flex: 9,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    Text: {
+        fontFamily: 'AveriaLibre-Regular',
+        fontSize: 30,
+        color: 'white',
+        margin: 15
     }
 });
